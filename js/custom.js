c6a8468c3f2e9635d55e72bad80383a95ebcc5f2
apple <apple@JO-iMac.skbroadband>
2024-03-08 11:40:25 +0900

    benefits section 작업중

--- js/custom.js
+++ js/custom.js
@@ -19,9 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
   // html 파일 내 요소들이 모두 로드되었을 때 실행
 
   // luxy.init
-  luxy.init({
-    wrapperSpeed: 0.9,
-  });
+  // luxy.init({
+  //   wrapperSpeed: 0.9,
+  // });
 
   // =========================== GSAP EFFECT ====================
 
@@ -55,6 +55,21 @@ document.addEventListener("DOMContentLoaded", function () {
       scrub: 1.8,
     },
   };
+
+  //square rotate animation
+  // const titleSquare = document.querySelector(".title-square");
+  const titleSquares = gsap.utils.toArray(".title-square");
+
+  titleSquares.forEach((square) => {
+    tl.from(square, {
+      scrollTrigger: {
+        trigger: square,
+        start: "top bottom",
+        scrub: 1.8,
+      },
+      rotate: 760,
+    });
+  });
 
   function headerAnimation(xValue) {
     // 이미지 애니메이션
@@ -103,6 +118,16 @@ document.addEventListener("DOMContentLoaded", function () {
       scrollTrigger: commonScrollTrigger.about,
       yPercent: 80,
     });
+
+    tl.from(".about-img img", {
+      scrollTrigger: commonScrollTrigger.about,
+      scale: 1.6,
+    });
+
+    tl.to(".about-text", {
+      scrollTrigger: commonScrollTrigger.about,
+      yPercent: 50,
+    });
   }
   aboutAnimation();
 
@@ -118,6 +143,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ===========================================
 });
+
+// const arr = ["홍콩반점", "오복성", "동보성"];
+
+// console.log(arr[0]);
+
+// const obj = {
+//   홍콩반점: [
+//     짜장면: 5000,
+//     짬뽕: 6000,
+//   ],
+//   오복성: ["짜장면", "짬뽕"],
+//   동보성: ["짜장면", "짬뽕"],
+// };
+
+// console.log(obj.홍콩반점[0]);
 
 // function abc(n, x) {
 //   return console.log(n + x);
